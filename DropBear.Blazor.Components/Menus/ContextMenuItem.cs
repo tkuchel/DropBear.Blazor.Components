7a9bc069e454dfe4091472a784de73374543c356
DropBear <tkuchel@users.noreply.github.com>
2024-07-18 13:39:14 +1000

    Added setters to the ContextMenuItem.cs

--- DropBear.Blazor.Components/Menus/ContextMenuItem.cs
+++ DropBear.Blazor.Components/Menus/ContextMenuItem.cs
@@ -2,11 +2,11 @@
 
 public class ContextMenuItem
 {
-    public string Text { get; } = "";
-    public string IconClass { get; } = "";
-    public bool IsSeparator { get; }
+    public string Text { get; set; } = "";
+    public string IconClass { get; set; } = "";
+    public bool IsSeparator { get; set; }
 #pragma warning disable CA1002
-    public List<ContextMenuItem>? SubmenuItems { get; } = [];
+    public List<ContextMenuItem>? SubmenuItems { get; set; } = [];
 #pragma warning restore CA1002
     public bool HasSubmenu => SubmenuItems is not null && SubmenuItems.Count is not 0;
 }
