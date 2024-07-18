05d59e500343aac73158e53ee0bad596a2455a7c
DropBear <tkuchel@users.noreply.github.com>
2024-07-18 13:17:28 +1000

    Fixed class accessibility issue.

--- DropBear.Blazor.Components/Menus/ContextMenuItem.cs
+++ DropBear.Blazor.Components/Menus/ContextMenuItem.cs
@@ -1,10 +1,10 @@
 ﻿namespace DropBear.Blazor.Components.Menus;
 
-public abstract class ContextMenuItem
+public class ContextMenuItem
 {
     public string Text { get; } = "";
     public string IconClass { get; } = "";
-    public bool IsSeparator { get; }
+    public bool IsSeparator { get; } = false;
 #pragma warning disable CA1002
     public List<ContextMenuItem>? SubmenuItems { get; } = [];
 #pragma warning restore CA1002
