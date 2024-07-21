000097762b05a4086fc14471a1dd9cf5bbd47f63
DropBear <tkuchel@users.noreply.github.com>
2024-07-21 11:07:58 +1000

    Incorporated new ratio based styling.
    Updated StandardCard.razor

--- DropBear.Blazor.Components/Components/Cards/StandardCard.razor.cs
+++ DropBear.Blazor.Components/Components/Cards/StandardCard.razor.cs
@@ -8,13 +8,14 @@ namespace DropBear.Blazor.Components.Components.Cards;
 
 public partial class StandardCard : ComponentBase
 {
-    [Parameter] public bool IsLightMode { get; set; }
-    [Parameter] public bool IsCompact { get; set; }
-    [Parameter] public string? ImgSrc { get; set; }
-    [Parameter] public string? ImgAlt { get; set; }
-    [Parameter] public string? Icon { get; set; }
+    [Parameter] public string ImgSrc { get; set; } = "https://via.placeholder.com/150";
+    [Parameter] public string ImgAlt { get; set; } = "Placeholder image";
+    [Parameter] public string Icon { get; set; } = "fas fa-ellipsis-h";
     [Parameter] public string Title { get; set; } = "Card Title";
-    [Parameter] public string? BodyTitle { get; set; }
+    [Parameter] public string BodyTitle { get; set; } = "Card Body Title";
     [Parameter] public RenderFragment? BodyContent { get; set; }
     [Parameter] public RenderFragment? FooterContent { get; set; }
+    [Parameter] public bool IsLightMode { get; set; }
+    [Parameter] public bool IsCompact { get; set; }
+    [Parameter] public bool EnableHover { get; set; } = true;
 }
